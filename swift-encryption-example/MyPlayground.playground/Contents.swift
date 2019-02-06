1741b0d494dafd271116bfeee3c5343418bc93aa
Peter Keller <petekeller2@gmail.com>
2019-02-05 20:57:01 -0500

    Add encrypt and decrypt functions

--- swift-encryption-example/MyPlayground.playground/Contents.swift
+++ swift-encryption-example/MyPlayground.playground/Contents.swift
@@ -2,16 +2,28 @@ import CryptoSwift
 
 let key = "<YOUR KEY>"
 let iv = "<YOUR IV>"
-let plaintext = "<YOUR PLAINTEXT>"
 
-do {
-    let aes = try AES(key: key.bytes, blockMode: CBC(iv: iv.bytes), padding: .pkcs5)
-    let ciphertext = try aes.encrypt(Array(plaintext.utf8))
-    if let base64Ciphertext = ciphertext.toBase64() {
-        print("ciphertext: \(base64Ciphertext)")
-    } else {
-        print("Error: Could not convert ciphertext to base64")
+func encrypt(plaintext: String) -> String {
+    do {
+        let aes = try AES(key: key.bytes, blockMode: CBC(iv: iv.bytes), padding: .pkcs5)
+        let ciphertext = try aes.encrypt(Array(plaintext.utf8))
+        if let base64Ciphertext = ciphertext.toBase64() {
+            return base64Ciphertext
+        } else {
+            print("Error: Could not convert ciphertext to base64")
+        }
+    } catch {
+        print("Error: \(error)")
     }
-} catch {
-    print("Error: \(error)")
+    return ""
+}
+
+func decrypt(encryptedText: String) -> String {
+    do {
+        let plaintext = try encryptedText.decryptBase64ToString(cipher: AES(key: key.bytes, blockMode: CBC(iv: iv.bytes), padding: .pkcs5))
+        return plaintext
+    } catch {
+        print("Error: \(error)")
+    }
+    return ""
 }
